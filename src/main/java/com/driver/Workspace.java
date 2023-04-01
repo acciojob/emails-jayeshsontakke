4d930a175474ba2c462d536dde8d6433e7fadadc
jayeshsontakke <jayeshsontakke1998@gmail.com>
2023-04-02 01:41:01 +0530

    code added

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
@@ -27,27 +29,24 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+       Collections.sort(calendar, new Comparator<Meeting>() {
+           @Override
+           public int compare(Meeting o1, Meeting o2) {
+              return o1.getEndTime().compareTo(o2.getEndTime());
 
-        Collections.sort(calendar,(o1,o2)->{
-            return o1.getStartTime()==o2.getStartTime()? o1.getEndTime().compareTo(o2.getEndTime()) :
-                    o1.getStartTime().compareTo(o2.getStartTime());
-        });
+              }
 
-        LocalTime limit = calendar.get(0).getEndTime();
+       });
+       int count=0;
+       LocalTime endTime=calendar.get(0).getEndTime();
+       for(int i=1;i< calendar.size();i++){
+           LocalTime startTime= calendar.get(i).getStartTime();
+           if(startTime.compareTo(endTime)>0){
+               count++;
+               endTime=calendar.get(i).getEndTime();
+           }
+       }
+         return count;
 
-        int maxMeeting = 0;
-
-        if(calendar.size()>0){
-            maxMeeting++;
-        }
-
-        for(int i=1; i< calendar.size(); i++){
-            if(calendar.get(i).getStartTime().compareTo(limit)>0)
-            {
-                maxMeeting++;
-                limit=calendar.get(i).getEndTime();
-            }
-        }
-        return 0;
     }
 }
